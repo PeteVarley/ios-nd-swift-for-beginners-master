e38013884644b9072cffddef0a69ee98bb8e3176
Peter Varley <petevarley@gmail.com>
2017-11-20 18:32:20 -0800

    Function Syntax Practice

--- Functions.playground/Pages/Function Syntax Practice.xcplaygroundpage/Contents.swift
+++ Functions.playground/Pages/Function Syntax Practice.xcplaygroundpage/Contents.swift
@@ -6,4 +6,10 @@
 //: Define a function called `myFirstFunction` that takes 0 parameters and prints something.
 //: **The solution is available on the next page!**
 
+func welcomeToLongBeach() {
+    print("Welcome to Long Beach!")
+}
+
+welcomeToLongBeach()
+
 //: [Next](@next)
