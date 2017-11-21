6b6104cfb80fb7a29eeefddedfe94640d74f8b84
Peter Varley <petevarley@gmail.com>
2017-11-20 18:48:36 -0800

    Functions with Params Practice

--- Functions.playground/Pages/Functions with Params Practice.xcplaygroundpage/Contents.swift
+++ Functions.playground/Pages/Functions with Params Practice.xcplaygroundpage/Contents.swift
@@ -6,8 +6,19 @@
 //: Define a function called `loveIt` that takes 1 `String` parameter called `subject` and prints "I ❤️ [subject]".
 //: **The solution is available on the next page!**
 
+func loveIt(subject:String){
+    print("I ❤️ \(subject)")
+}
+
+loveIt(subject:"dogs")
+
 //: ### Exercise 2
 //: Define a function called `shout` that takes 1 `String` parameter called `subject` and prints "[subject]❗️".
 //: **The solution is available on the next page!**
 
+func shout(subject:String){
+    print("\(subject)❗️")
+}
+
+shout(subject: "That dog got loose")
 //: [Next](@next)
